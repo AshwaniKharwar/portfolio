0702be573f18a98c4f71f95b6c90ce6a47b6a212
Ashwani Kharwar <ashwanikumarkharwar83@gmail.com>
2024-08-05 14:55:00 +0530

    portfolio

---
+++ src/components/Projects/index.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import { Container, Wrapper, Title, Desc, CardContainer, ToggleButtonGroup, ToggleButton, Divider } from './ProjectsStyle'
+import ProjectCard from '../Cards/ProjectCards'
+import { projects } from '../../data/constants'
+
+
+const Projects = ({openModal,setOpenModal}) => {
+  return (
+    <Container id="projects">
+      <Wrapper>
+        <Title>Projects</Title>
+        <Desc>
+          I have worked on a wide range of projects. Here are some of my projects.
+        </Desc>
+        <CardContainer>
+          {projects
+            .map((project, index) => (
+              <ProjectCard key={index} project={project} openModal={openModal} setOpenModal={setOpenModal}/>
+            ))}
+        </CardContainer>
+      </Wrapper>
+    </Container>
+  )
+}
+
+export default Projects
